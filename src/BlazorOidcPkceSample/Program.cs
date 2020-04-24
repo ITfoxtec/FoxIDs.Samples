7027b7c57ffd06a41d5d19117cf7f359e8dd2551
Anders Revsgaard <anders@itfoxtec.com>
2020-04-24 15:16:49 +0200

    Updated to Blazor WebAssembly 3.2.0 Preview 5.

--- src/BlazorOidcPkceSample/Program.cs
+++ src/BlazorOidcPkceSample/Program.cs
@@ -15,36 +15,12 @@ namespace BlazorOidcPkceSample
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            //builder.Services.AddSingleton(sp =>
-            //{
-            //    return new HttpClient(sp.GetRequiredService<AuthorizationMessageHandler>()
-            //        .ConfigureHandler(
-            //          new[] { "https://localhost:44330/testcorp/dev/blazor_oidcauthcodepkce_sample(login)/" },
-            //          scopes: new[] { "openid", "profile", "aspnetcore_api1_sample:some_access" }))
-            //    {
-            //        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            //    };
-            //});
-
             var httpClientLogicalName = "BlazorOidcPkceSample.ServerAPI";
             builder.Services.AddHttpClient(httpClientLogicalName, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(httpClientLogicalName));
-
-            //builder.Services.AddOidcAuthentication(options =>
-            //{
-            //    // Configure your authentication provider options here.
-            //    // For more information, see https://aka.ms/blazor-standalone-auth
-            //    options.ProviderOptions.Authority = "https://localhost:44330/testcorp/dev/blazor_oidcauthcodepkce_sample(login)/";
-            //    options.ProviderOptions.ClientId = "blazor_oidcauthcodepkce_sample";
-            //    options.ProviderOptions.DefaultScopes.Add("aspnetcore_api1_sample:some_access");
-            //    options.ProviderOptions.ResponseType = "code";
-            //});
-
-            //builder.Services.AddOidcAuthentication(options =>
-            //    builder.Configuration.Bind("IdentitySettings", options.ProviderOptions));
 
             builder.Services.AddOidcAuthentication(options =>
             {

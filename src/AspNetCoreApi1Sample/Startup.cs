3a1d1d079b4413c5b9fc6b32e49a414d3ee9ade7
Anders Revsgaard <anders@itfoxtec.com>
2020-11-17 22:12:14 +0100

    Sample API UseDeveloperExceptionPage removed.

--- src/AspNetCoreApi1Sample/Startup.cs
+++ src/AspNetCoreApi1Sample/Startup.cs
@@ -67,11 +67,7 @@ namespace AspNetCoreApi1Sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            if (!env.IsDevelopment())
             {
                 app.UseHsts();
             }

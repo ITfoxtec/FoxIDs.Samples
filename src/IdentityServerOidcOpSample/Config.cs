ef1c6f608971331bcfaac817082b4c3784f595e3
Anders Revsgaard <anders@itfoxtec.com>
2021-02-28 21:12:50 +0100

    IdentityServerOidcOpSample start.

---
+++ src/IdentityServerOidcOpSample/Config.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("foxids_identityserver.api", "FoxIDs API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "foxids_identityserver",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("2tqjW-KwiGaR4KRt0IJ8KAJYw3pyPTK8S_dr_YE5nbw".Sha256())
+                    },
+
+                    AllowedScopes = { "foxids_identityserver" }
+                }
+            };
+    }
+}
---
+++ src/IdentityServerOidcOpSample/Startup.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // To configure Identity Server to use the standard when emitting scopes into a JWT token.
+                options.EmitScopesAsSpaceDelimitedStringInJwt = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}

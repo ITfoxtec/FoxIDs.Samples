557461d6762f5c47155ca264d3f82529353201d6
Anders Revsgaard <anders@itfoxtec.com>
2021-03-05 10:31:50 +0100

    Change name to parallel FoxIDs track instead of alternative

--- src/AspNetCoreOidcAuthorizationCodeSample/Models/IdentitySettings.cs
+++ src/AspNetCoreOidcAuthorizationCodeSample/Models/IdentitySettings.cs
@@ -8,7 +8,7 @@ namespace AspNetCoreOidcAuthorizationCodeSample.Models
         public string ClientSecret { get; set; }
 
         public string FoxIDsLoginUpParty { get; set; }
-        public string AlternativeFoxIDsUpParty { get; set; }
+        public string ParallelFoxIDsUpParty { get; set; }
         public string IdentityServerUpParty { get; set; }
         public string SamlIdPSampleUpParty { get; set; }
         public string SamlIdPAdfsUpParty { get; set; }

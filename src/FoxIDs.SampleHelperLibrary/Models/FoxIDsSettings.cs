a2682513ea2eb8fccb3fedf58857a7dac140759c
Anders Revsgaard <anders@itfoxtec.com>
2022-06-17 21:46:29 +0200

    Support custom domains.

--- src/FoxIDs.SampleHelperLibrary/Models/FoxIDsSettings.cs
+++ src/FoxIDs.SampleHelperLibrary/Models/FoxIDsSettings.cs
@@ -4,9 +4,10 @@ namespace FoxIDs.SampleHelperLibrary.Models
 {
     public class FoxIDsSettings
     {
-        public string Authority => FoxIDsEndpoint == null ? null : UrlCombine.Combine(FoxIDsEndpoint, Tenant, Track, DownParty);
+        public string Authority => FoxIDsEndpoint == null ? null : (IncludeTenantInUrl ? UrlCombine.Combine(FoxIDsEndpoint, Tenant, Track, DownParty) : UrlCombine.Combine(FoxIDsEndpoint, Track, DownParty));
 
         public string FoxIDsEndpoint { get; set; }
+        public bool IncludeTenantInUrl { get; set; }
         public string Tenant { get; set; }
         public string Track { get; set; }
         public string DownParty { get; set; }

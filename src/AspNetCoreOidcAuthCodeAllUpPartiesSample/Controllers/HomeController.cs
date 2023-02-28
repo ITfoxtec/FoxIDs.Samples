3920c67f81d6b76117f6b5e834ac79125b625590
Anders Revsgaard <anders@itfoxtec.com>
2023-02-28 10:12:24 +0100

    clean up

--- src/AspNetCoreOidcAuthCodeAllUpPartiesSample/Controllers/HomeController.cs
+++ src/AspNetCoreOidcAuthCodeAllUpPartiesSample/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System.Diagnostics;
-using System.Net.Http;
 
 namespace AspNetCoreOidcAuthCodeAllUpPartiesSample.Controllers
 {

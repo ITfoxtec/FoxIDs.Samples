8100f7fa40930dd21679fba10acea215fb52ab40
Anders Revsgaard <anders@itfoxtec.com>
2023-03-14 10:18:45 +0100

    Show how to call the CallUserInfo endpoint

--- src/AspNetCoreOidcAuthCodeAllUpPartiesSample/Controllers/HomeController.cs
+++ src/AspNetCoreOidcAuthCodeAllUpPartiesSample/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using AspNetCoreOidcAuthCodeAllUpPartiesSample.Models;
+using ITfoxtec.Identity;
+using ITfoxtec.Identity.Discovery;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 namespace AspNetCoreOidcAuthCodeAllUpPartiesSample.Controllers
@@ -10,11 +14,13 @@ namespace AspNetCoreOidcAuthCodeAllUpPartiesSample.Controllers
     public class HomeController : Controller
     {
         private readonly AppSettings appSettings;
+        private readonly OidcDiscoveryHandler oidcDiscoveryHandler;
         private readonly IHttpClientFactory httpClientFactory;
 
-        public HomeController(AppSettings appSettings, IHttpClientFactory httpClientFactory)
+        public HomeController(AppSettings appSettings, OidcDiscoveryHandler oidcDiscoveryHandler, IHttpClientFactory httpClientFactory)
         {
             this.appSettings = appSettings;
+            this.oidcDiscoveryHandler = oidcDiscoveryHandler;
             this.httpClientFactory = httpClientFactory;
         }
 
@@ -45,6 +51,29 @@ namespace AspNetCoreOidcAuthCodeAllUpPartiesSample.Controllers
             }
 
             ViewBag.Title = "Call AspNetCoreApi1Sample";
+            return View("CallApi");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CallUserInfo()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var oidcDiscovery = await oidcDiscoveryHandler.GetOidcDiscoveryAsync();
+
+            using var response = await httpClientFactory.CreateClient().GetAsync(oidcDiscovery.UserInfoEndpoint, accessToken);
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Result = JToken.Parse(await response.Content.ReadAsStringAsync()).ToString();
+            }
+            else
+            {
+                ViewBag.Result = $"Unable to call API. API URL='{oidcDiscovery.UserInfoEndpoint}', StatusCode='{response.StatusCode}'";
+
+                var wwwAuthenticateHeader = string.Join(" ", response.Headers.WwwAuthenticate.Select(h => $"{h.Scheme}{(h.Parameter.IsNullOrWhiteSpace() ? string.Empty : $"='{h.Parameter}'")}"));
+                ViewBag.Result = $"{ViewBag.Result}{Environment.NewLine}WWWAuthenticate header: {wwwAuthenticateHeader}";
+            }
+
+            ViewBag.Title = "Call UserInfo endpoint";
             return View("CallApi");
         }
 

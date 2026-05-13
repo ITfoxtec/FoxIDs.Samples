62da33c8731f9c754a1307436a0767c2bd021e80
Anders Revsgaard <anders@itfoxtec.com>
2026-05-13 11:13:44 +0200

    Improve response errors in Directory Connector

--- src/DirectoryConnectorApiSample/Models/Api/DirectorySetPasswordRequest.cs
+++ src/DirectoryConnectorApiSample/Models/Api/DirectorySetPasswordRequest.cs
@@ -4,6 +4,8 @@ namespace DirectoryConnectorApiSample.Models.Api;
 
 public class DirectorySetPasswordRequest : DirectoryUserIdentifierRequest
 {
+    protected override bool RequireDirectoryUserId => true;
+
     [Required]
     public string Password { get; set; }
 }
